1803d64ac265ecdca1b10432b643853a5e3442e4
Luis Pino <lpino@darient.com>
2019-11-12 13:00:37 -0400

    [Samuel] adding assets

--- src/components/Nominaciones/index.tsx
+++ src/components/Nominaciones/index.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import styles from './styles'
+import { Typography } from '@material-ui/core'
 
 import { withStyles, CssBaseline } from "@material-ui/core";
 import { connect } from "react-redux";
 import * as usuariosActions from "../../actions/usuariosActions";
 
 const Nominaciones = (props: any) => {
-  const { classes } = props;
 
   return (
       <React.Fragment>
         <CssBaseline />
+        <div style={{height: '40px'}}>
+          <Typography>asfasfa</Typography>
+        </div>
         <div>
-          <div style={{height: '50vh'}}>
-
+          <div style={{height: '40vh', width: '100%', background: 'blue'}}>
+            <div style={{width: '20px', height: '20px'}}>
+              aasafas
+            </div>
           </div>
-          <div style={{height: '50vh'}}>
+          <div style={{height: '60vh'}}>
 
           </div>
         </div>
